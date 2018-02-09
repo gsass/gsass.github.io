0aa1514a02c8c0056a3cf61d0706329698a62a3b
George Sass <george.a.sass@gmail.com>
2018-02-08 20:51:04 -0500

    Accidentally a word.

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ window.onload = function () {
 I'm George. I make software, usually to make the internal tools and web store at [Warby Parker](https://www.warbyparker.com) better. Currently I work across our <small>(Python/JS–ES6,&nbsp;React,&nbsp;Redux, PSQL, AWS)</small> web stack. Yep, I wear glasses.
 Before glasses, I worked on making [Electronic Health Records](https://www.vecna.com/) more useful and accessible. Way back when, I wrote code to run robots and test sensors. I still like hardware a bunch.
 Outside of work, I like to make stuff, mostly food, bikes, and ceramics. I also like riding the bikes.
-I have cat. His name's Lorenzo.
+I have a cat. His name's Lorenzo.
 
 🍊`
       }
